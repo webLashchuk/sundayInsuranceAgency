ecbae368f0bb190dff46be025c26a3c3104fe1bd
Illia Lashchuk <chernyadyeviv@gmail.com>
2024-07-10 17:38:03 +0300

    added article page, and block of article in education page

--- src/js/components/_carousel.js
+++ src/js/components/_carousel.js
@@ -1,8 +1,25 @@
-const articlesItem = document.querySelectorAll('.articles__item');
+const articlesItem = document.querySelectorAll('.article-carousel__item');
 
 articlesItem.forEach(item => {
   item.addEventListener('mouseover', () => {
-    articlesItem.forEach(i => i.classList.remove('active'));
+    articlesItem.forEach(i =>
+      i.classList.remove(
+        'active',
+        'inactive-left',
+        'inactive-right',
+        'inactive'
+      )
+    );
     item.classList.add('active');
+
+    const activeIndex = Array.from(articlesItem).indexOf(item);
+
+    articlesItem.forEach((i, index) => {
+      if (index < activeIndex) {
+        i.classList.add('inactive-left');
+      } else if (index > activeIndex) {
+        i.classList.add('inactive-right');
+      }
+    });
   });
 });
