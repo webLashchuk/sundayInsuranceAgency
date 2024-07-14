4f7b3c0b95e1e7edaec3b81e3776784e368992e2
Illia Lashchuk <chernyadyeviv@gmail.com>
2024-07-14 03:10:45 +0300

    completed desktop design

--- src/js/components/_carousel.js
+++ src/js/components/_carousel.js
@@ -1,25 +1,31 @@
 const articlesItem = document.querySelectorAll('.article-carousel__item');
+const productsItem = document.querySelectorAll('.products__item');
 
-articlesItem.forEach(item => {
-  item.addEventListener('mouseover', () => {
-    articlesItem.forEach(i =>
-      i.classList.remove(
-        'active',
-        'inactive-left',
-        'inactive-right',
-        'inactive'
-      )
-    );
-    item.classList.add('active');
+function addActiveClass(itemBlock) {
+  itemBlock.forEach(item => {
+    item.addEventListener('mouseover', () => {
+      itemBlock.forEach(i =>
+        i.classList.remove(
+          'active',
+          'inactive-left',
+          'inactive-right',
+          'inactive'
+        )
+      );
+      item.classList.add('active');
 
-    const activeIndex = Array.from(articlesItem).indexOf(item);
+      const activeIndex = Array.from(articlesItem).indexOf(item);
 
-    articlesItem.forEach((i, index) => {
-      if (index < activeIndex) {
-        i.classList.add('inactive-left');
-      } else if (index > activeIndex) {
-        i.classList.add('inactive-right');
-      }
+      itemBlock.forEach((i, index) => {
+        if (index < activeIndex) {
+          i.classList.add('inactive-left');
+        } else if (index > activeIndex) {
+          i.classList.add('inactive-right');
+        }
+      });
     });
   });
-});
+}
+
+addActiveClass(articlesItem);
+addActiveClass(productsItem);
