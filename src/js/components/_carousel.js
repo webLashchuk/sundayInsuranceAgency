99c459481cf0830393651a81fc9f94f068035419
Illia Lashchuk <chernyadyeviv@gmail.com>
2024-07-16 09:55:01 +0300

    modified js-carousel

--- src/js/components/_carousel.js
+++ src/js/components/_carousel.js
@@ -1,31 +1,91 @@
 const articlesItem = document.querySelectorAll('.article-carousel__item');
 const productsItem = document.querySelectorAll('.products__item');
 
-function addActiveClass(itemBlock) {
+function addActiveClass(itemBlock, shouldExpandOnFirstClick = false) {
   itemBlock.forEach(item => {
-    item.addEventListener('mouseover', () => {
-      itemBlock.forEach(i =>
-        i.classList.remove(
-          'active',
-          'inactive-left',
-          'inactive-right',
-          'inactive'
-        )
-      );
-      item.classList.add('active');
+    const link = item.querySelector('a');
+    if (!shouldExpandOnFirstClick) {
+      item.setAttribute('data-expanded', 'false');
 
-      const activeIndex = Array.from(articlesItem).indexOf(item);
+      item.addEventListener('click', event => {
+        if (item.getAttribute('data-expanded') === 'false') {
+          event.preventDefault();
+          item.setAttribute('data-expanded', 'true');
 
-      itemBlock.forEach((i, index) => {
-        if (index < activeIndex) {
-          i.classList.add('inactive-left');
-        } else if (index > activeIndex) {
-          i.classList.add('inactive-right');
+          itemBlock.forEach(i => {
+            i.classList.remove(
+              'active',
+              'inactive-left',
+              'inactive-right',
+              'inactive'
+            );
+            i.setAttribute('data-expanded', 'false');
+          });
+          item.classList.add('active');
+
+          const activeIndex = Array.from(itemBlock).indexOf(item);
+
+          itemBlock.forEach((i, index) => {
+            if (index < activeIndex) {
+              i.classList.add('inactive-left');
+            } else if (index > activeIndex) {
+              i.classList.add('inactive-right');
+            }
+          });
+
+          item.setAttribute('data-expanded', 'true');
         }
       });
-    });
+    } else {
+      link.addEventListener('click', () => {});
+
+      item.addEventListener('mouseover', () => {
+        itemBlock.forEach(i =>
+          i.classList.remove(
+            'active',
+            'inactive-left',
+            'inactive-right',
+            'inactive'
+          )
+        );
+        item.classList.add('active');
+
+        const activeIndex = Array.from(itemBlock).indexOf(item);
+
+        itemBlock.forEach((i, index) => {
+          if (index < activeIndex) {
+            i.classList.add('inactive-left');
+          } else if (index > activeIndex) {
+            i.classList.add('inactive-right');
+          }
+        });
+      });
+    }
   });
 }
 
-addActiveClass(articlesItem);
-addActiveClass(productsItem);
+function updateBehaviorBasedOnScreenWidth() {
+  const screenWidth = window.innerWidth;
+
+  if (screenWidth < 992) {
+    addActiveClass(articlesItem);
+  } else {
+    addActiveClass(articlesItem, true);
+  }
+
+  addActiveClass(productsItem, true);
+}
+
+updateBehaviorBasedOnScreenWidth();
+
+window.addEventListener('resize', () => {
+  articlesItem.forEach(item => {
+    item.replaceWith(item.cloneNode(true));
+  });
+
+  productsItem.forEach(item => {
+    item.replaceWith(item.cloneNode(true));
+  });
+
+  updateBehaviorBasedOnScreenWidth();
+});
